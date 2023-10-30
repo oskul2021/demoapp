90b055d80075a03d7f74967538a9c5e8ea4febfc
oskul2021 <albertlionelmalang@gmail.com>
2023-10-30 13:27:23 +0100

    first init

---
+++ src/main/java/com/beconcept/demoapp/HelloController.java
@@ -0,0 +1,12 @@
+package com.beconcept.demoapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "You are welcome";
+    }
+}
---
+++ src/test/java/com/beconcept/demoapp/DemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beconcept.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
